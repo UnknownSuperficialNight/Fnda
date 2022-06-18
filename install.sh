71cbfeb6922a17016d32b72aecb5a17e5b305e9e
UnknownSuperficialNight <88142731+UnknownSuperficialNight@users.noreply.github.com>
2022-06-19 06:01:24 +1200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+path=$(pwd)
+sudo wget https://github.com/UnknownSuperficialNight/Fnda/blob/main/fnda.sh.x && sudo chmod +x fnda.sh.x && clear && ln -s $path/fnda.sh.x $HOME/.local/bin/fda
+
+echo
+echo /////////////////////////////////
+echo
+echo complete installation
+echo
+echo /////////////////////////////////
+echo
